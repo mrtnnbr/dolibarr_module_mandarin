1a533ca8a2ff3611349cb4beb9591a4a9da4ba72
Alexis Algoud <alexis@atm-consulting.fr>
2016-02-08 13:05:07 +0100

    FIX graph projet

--- graph_project.php
+++ graph_project.php
@@ -10,52 +10,55 @@
 	$fk_statut = GETPOST('fk_statut', 'int');
 	
 	$sql = 'SELECT p.rowid, p.title, pt.rowid AS fk_task
-			, pt.planned_workload AS temps_prevu
-			, pt.planned_workload * (pt.progress / 100) AS temps_theorique
-			, SUM(ptt.task_duration) AS temps_reel
-			FROM llx_projet p
-			LEFT JOIN llx_projet_task pt ON (p.rowid = pt.fk_projet)
-			LEFT JOIN llx_projet_task_time ptt ON (pt.rowid = ptt.fk_task)
-			WHERE p.entity = 1';
+			, (SELECT SUM(task_duration) 
+				FROM '.MAIN_DB_PREFIX.'projet_task_time ptt
+				LEFT JOIN '.MAIN_DB_PREFIX.'projet_task t ON (t.rowid = ptt.fk_task)
+				WHERE t.fk_projet = p.rowid) AS temps_reel
+			, SUM(pt.planned_workload) AS temps_prevu
+			, SUM(pt.planned_workload * (pt.progress / 100)) AS temps_theorique
+			FROM '.MAIN_DB_PREFIX.'projet p
+			LEFT JOIN '.MAIN_DB_PREFIX.'projet_task pt ON (p.rowid = pt.fk_projet)
+			WHERE p.entity = '.$conf->entity;
 
 	if (!empty($fk_statut)) $sql .= ' AND p.fk_statut = '.$fk_statut;
-	$sql .= ' GROUP BY pt.rowid ORDER BY p.title';
+	$sql .= ' GROUP BY p.rowid 
+	ORDER BY p.title';
 
 	$resql = $db->query($sql);
+	
 	if ($resql)
 	{
 		while ($line = $db->fetch_object($resql))
 		{
 			
-			if (!isset($TData[$line->rowid]))
-			{
-				$TData[$line->rowid] = array(
-					'name'=>dol_escape_js($line->title)
+			$TData[$line->rowid] = array(
+					'name'=>$line->title
 					,'temps_prevu' => $line->temps_prevu
 					,'Progression réelle'=>$line->temps_reel
 					,'Progression théorique'=> $line->temps_theorique
 				);
-			}
-			else
-			{
-				$TData[$line->rowid]['temps_prevu'] += $line->temps_prevu;
-				$TData[$line->rowid]['Progression réelle'] += $line->temps_reel;
-				$TData[$line->rowid]['Progression théorique'] += $line->temps_theorique;
-			}
+			
 		}
 
-		foreach ($TData as &$Tab)
+		$TDataTransform = array();
+		foreach ($TData as $k=>&$Tab)
 		{
 			$temps_prevu = $Tab['temps_prevu'];
-			unset($Tab['temps_prevu']);
+			//unset($Tab['temps_prevu']);
 			
-			if (empty($temps_prevu)) unset($Tab);
+			if (empty($temps_prevu)){
+				continue;	
+			}
 			
-			$Tab['Progression réelle'] = ($Tab['Progression réelle'] * 100) / $temps_prevu;
-			$Tab['Progression théorique'] = ($Tab['Progression théorique'] * 100) / $temps_prevu;
+			$TDataTransform[] = array(
+				'name'=>dol_escape_js($Tab['name'])
+				
+				,'Progression réelle' => $temps_prevu>0 ? round($Tab['Progression réelle'] * 100 / $temps_prevu) : 100
+				,'Progression théorique' => $temps_prevu > 0 ? round($Tab['Progression théorique'] * 100 / $temps_prevu) : 100
+			);
 		}
 	}
-	
+	//var_dump($TDataTransform);
 	// Begin of page
 	llxHeader('', $langs->trans('mandarinTitleGraphProjet'), '');
 	
@@ -63,7 +66,7 @@
 	$explorer->actions = array("dragToZoom", "rightClickToReset");
 	
 	$listeview = new TListviewTBS('graphProject');
-	print $listeview->renderArray($PDOdb, $TData
+	print $listeview->renderArray($PDOdb, $TDataTransform
 		,array(
 			'type' => 'chart'
 			,'chartType' => 'ColumnChart'
